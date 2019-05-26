0c4e054fd768a2594f4c6464b02bc2d3e0bf4d61
Raquel Coriat <rcoriat14@gmail.com>
2019-05-26 17:35:35 -0400

    Controlador/rutas usuario con funciones para auth

--- server/routes/usuarioRoutes.js
+++ server/routes/usuarioRoutes.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+
 const Usuario = require("../controllers/usuarioController");
 
 //Traer todos los usuarios
-router.get('/', Usuario.findAll);
+//router.get('/', Usuario.findAll);
 
-//Crear un usuario
+//Registrar un usuario
 router.post('/crear', Usuario.create);
+
+//Login de un usuario
+router.post('/auth', Usuario.auth);
+
+//Falta verificar uso
+router.get('/profile', Usuario.profile);
 
 //Buscar un usuario por correo
 router.post('/buscar', Usuario.findByEmail);
 
 //"Autenticar" un usuario 
-router.post('/auth', Usuario.auth);
+//router.post('/auth', Usuario.auth);
 
 //Editar un usuario
 router.put('/editar/:id_usuario', Usuario.update);
